40d6565b1a76c1ff47f65d1c2c3fd547194ad8b0
onosannnnt <onopingping@gmail.com>
2024-11-12 02:49:43 +0700

    fix : change userID to user_ID

--- src/serverAction.tsx
+++ src/serverAction.tsx
@@ -24,9 +24,9 @@ export const addUser = async (formData: FormData) => {
 export const handlePopClick = async () => {
   const cookieStore = await cookies();
   const userID = cookieStore.get("user_id");
-  await fetch("http://localhost:3000/api/score", {
+  await fetch("http://localhost:3000/api/score/", {
     method: "POST",
-    body: JSON.stringify({ userID: userID?.value }),
+    body: JSON.stringify({ user_ID: userID?.value }),
     headers: {
       "Content-Type": "application/json",
     },
