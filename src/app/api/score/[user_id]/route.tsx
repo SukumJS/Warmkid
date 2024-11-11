b92815082a9ac0ca3af16eb4723c67fcca46c4df
onosannnnt <onopingping@gmail.com>
2024-11-12 02:33:16 +0700

    create pop click button and imprement get route

--- src/app/api/score/[user_id]/route.tsx
+++ src/app/api/score/[user_id]/route.tsx
@@ -1,15 +1,17 @@
 import { NextResponse } from "next/server";
 import { ConnectDB } from "../../../../../config/config";
 import { ClickModel } from "../../../../../models/models";
+
 export async function GET(
   req: Request,
   { params }: { params: { user_id: string } }
 ) {
   try {
-    const { user_id } = params;
-    ConnectDB();
+    const { user_id } = await params;
+    console.log(user_id);
+    await ConnectDB();
     const response = await ClickModel.countDocuments({
-      user_id,
+      user_id: Object(user_id),
     });
     return NextResponse.json({
       user_id: user_id,
