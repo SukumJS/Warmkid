931bce5e67326cf3efa7ec880e9681e29c7057bc
pepperpedmak <127705037+pepperpedmak@users.noreply.github.com>
2024-11-13 09:11:47 +0700

    total Score page

--- src/app/leaderBoard/page.tsx
+++ src/app/leaderBoard/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useState , useEffect} from 'react';
-const [click, setClickt] = useState(0);
 
 export default function App() {
+    
     return(
         <>
-        
+            
         </>
     );
 }
--- src/serverAction.tsx
+++ src/serverAction.tsx
@@ -1,4 +1,5 @@
 "use server";
+import { METHODS } from "http";
 import { cookies } from "next/headers";
 
 export const addUser = async (formData: FormData) => {
@@ -52,3 +53,19 @@ export const getScore = async () => {
   return data.user.counter; // Assuming the response has a score property
 };
 
+export const getTotalScore = async () => {
+  const response = await fetch("http://localhost:3000/api/score/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    console.error("Failed to fetch total score");
+    return 0;
+  }
+
+  const data = await response.json();
+  return data.total; 
+};
