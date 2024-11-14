f5693a29756f66b311ff0afba1d85fb2a332b9a6
SukumJS <sukumpat2547@gmail.com>
2024-11-14 14:30:51 +0700

    include image data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,94 @@
 "use client";
 
-import React, { useState , useEffect } from "react";
-import { addUser , checkCoookie } from "@/serverAction";
+import React, { useState, useEffect } from "react";
+import { addUser, checkCoookie } from "@/serverAction";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-
-  const[cookie , setCookie] = useState(false);
+  const [cookie, setCookie] = useState(false);
 
   useEffect(() => {
     async function getCookie() {
       const isCookie = await checkCoookie();
-      if(isCookie)setCookie(true);
+      if (isCookie) setCookie(true);
     }
-   getCookie();
-   }, []);
+    getCookie();
+  }, []);
 
-  if(!cookie){
+  if (!cookie) {
     return (
       <>
-        <div className="bg w-screen h-screen py-10 px-5 justify-items-center relative"> 
-          <Image src="/img/logoWarmkids.svg" alt="" width={500} height={500} className="mb-4"/>
-          <form action={addUser} className="text-xl">
-            <input type="name" name="name" placeholder="Enter name" className="mr-2 text-black px-1 rounded"/>
-            <button className="bg-green-400 text-white px-1 rounded">submit</button>
+        <div className="bg w-screen h-screen py-10 px-5 justify-items-center relative">
+          <Image
+            src="/img/logoWarmkids.svg"
+            alt=""
+            width={500}
+            height={500}
+            className="mb-4"
+          />
+          <form
+            action={addUser}
+            className="py-40 text-xl flex flex-col items-center justify-center "
+          >
+            <input
+              type="name"
+              name="name"
+              placeholder="Enter name"
+              className="mb-4 text-black px-2 py-1 rounded"
+            />
+            <button className="bg-green-400 text-white px-4 py-2 rounded">
+              Submit
+            </button>
           </form>
-          <Image src="/img/logobottom.svg" alt="" width={500} height={500} className="absolute bottom-0 left-1/2 transform -translate-x-1/2"/>
+          <Image
+            src="/img/logobottom.svg"
+            alt=""
+            width={500}
+            height={500}
+            className="absolute bottom-0 left-1/2 transform -translate-x-1/2"
+          />
         </div>
       </>
     );
   } else {
-    return(
+    return (
       <>
-      <div className="bg w-screen h-screen py-10 px-5 flex-col justify-items-center"> 
-        <Image src="/img/logoWarmkids.svg" alt="" width={500} height={500} className="mb-4" />
-        <Link href={"/gameone"}><Image src="/img/gameone8.svg" alt="logoG1" width={300} height={300} className="mb-6" /></Link>
-        <Link href={"/modpop"}><Image src="/img/logoGame2.svg" alt="logoG2" width={300} height={300} className="mb-6" /></Link>
-        <Image src="/img/logobottom.svg" alt="" width={500} height={500} className="absolute bottom-0 left-1/2 transform -translate-x-1/2"/>
-      </div>
+        <div className="bg w-screen h-screen py-10 px-5 flex-col justify-items-center">
+          <Image
+            src="/img/logoWarmkids.svg"
+            alt=""
+            width={500}
+            height={500}
+            className="mb-4"
+          />
+          <Link href={"/gameone"}>
+            <Image
+              src="/img/gameone8.svg"
+              alt="logoG1"
+              width={300}
+              height={300}
+              className="mb-6"
+            />
+          </Link>
+          <Link href={"/modpop"}>
+            <Image
+              src="/img/logoGame2.svg"
+              alt="logoG2"
+              width={300}
+              height={300}
+              className="mb-6"
+            />
+          </Link>
+          <Image
+            src="/img/logobottom.svg"
+            alt=""
+            width={500}
+            height={500}
+            className="absolute bottom-0 left-1/2 transform -translate-x-1/2"
+          />
+        </div>
       </>
     );
   }
-  
 }
